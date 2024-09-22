a114898f1e0476f6941d30dcfc54188f441b2e64
RayhanSohel <arayhansohel@gmail.com>
2024-09-23 04:45:15 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// ------Custom Tailwind CSS------//
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          brandColor: '#00935F',
+          bgColor: '#00935F69',
+          textColor: '#494949',
+        },
+        fontFamily: {
+          manrope: ['Manrope'],
+        }
+      }
+    }
+  }
