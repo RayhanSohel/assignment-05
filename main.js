d79f3dabf53a207f65b6522114b254b4bc2bb35e
RayhanSohel <arayhansohel@gmail.com>
2024-09-24 11:37:58 +0600

    added switching button

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
 // ------Custom Tailwind CSS------//
 tailwind.config = {
     theme: {
       extend: {
         colors: {
           brandColor: '#00935F',
-          bgColor: '#00935F69',
+          bgColor: '#00935F33',
           textColor: '#494949',
         },
         fontFamily: {
@@ -14,3 +13,25 @@ tailwind.config = {
       }
     }
   }
+
+// ------Switching Menu Button-----//
+function setActive(link) {
+  const buttons = document.querySelectorAll('.menu-btn');
+  buttons.forEach(btn => {
+      btn.classList.remove('btn-success');
+      btn.classList.add('btn');
+  });
+  link.classList.add('btn-success');
+
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+      section.classList.add('hidden');
+  });
+
+  const activeSection = document.getElementById(link.dataset.target);
+  activeSection.classList.remove('hidden');
+}
+window.onload = function() {
+  const defaultButton = document.querySelector('.menu-btn[data-target="donation-section"]');
+  setActive(defaultButton);
+};
